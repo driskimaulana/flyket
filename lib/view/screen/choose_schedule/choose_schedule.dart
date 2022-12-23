ca863856d2e6f671843e0e67f3c67fac34ae92b2
D'Riski Maulana <68141563+driskimaulana@users.noreply.github.com>
2022-12-23 20:45:32 +0700

    intial project

---
+++ lib/view/screen/choose_schedule/choose_schedule.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChooseSchedule extends StatefulWidget {
+  const ChooseSchedule({super.key});
+
+  @override
+  State<ChooseSchedule> createState() => _ChooseScheduleState();
+}
+
+class _ChooseScheduleState extends State<ChooseSchedule> {
+  @override
+  Widget build(BuildContext context) {
+    // PAKE LIST VIEW BUILDER
+
+    return Container();
+  }
+}
---
+++ lib/view/screen/transaction_history/transaction_history.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistory extends StatefulWidget {
+  const TransactionHistory({super.key});
+
+  @override
+  State<TransactionHistory> createState() => _TransactionHistoryState();
+}
+
+class _TransactionHistoryState extends State<TransactionHistory> {
+  @override
+  Widget build(BuildContext context) {
+    // PAKE LIST VIEW BUILDER
+
+    return Container();
+  }
+}
