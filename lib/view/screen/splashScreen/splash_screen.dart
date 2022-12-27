c276e9324017e7312332268c421fef94dae69db3
D'Riski Maulana <68141563+driskimaulana@users.noreply.github.com>
2022-12-27 16:59:22 +0700

    splash screen and onboarding screen

---
+++ lib/view/screen/splashScreen/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flyket/view/screen/onboarding/Onboarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const Onboarding()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 1, 113, 128),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/flyket.png",
+              height: 50,
+              width: 100,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SpinKitChasingDots(
+              size: 20,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
